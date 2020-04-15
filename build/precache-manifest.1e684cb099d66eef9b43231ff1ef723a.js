1b82cef8e8f1afd0e868ce1b1c36c97ee34b7097
Mia Erbus <mia.erbus@gmail.com>
2020-04-15 10:12:57 +0200

    uibuild

---
+++ build/precache-manifest.1e684cb099d66eef9b43231ff1ef723a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbedd4ad08fb9aeb32a7a123675d9ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ec2e381debc0a9f10e4",
+    "url": "/static/css/main.b5adea53.chunk.css"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec2e381debc0a9f10e4",
+    "url": "/static/js/main.f7bd87a5.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
