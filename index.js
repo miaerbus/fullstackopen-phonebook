c4adc90f6f642f9a1557e39405560e9ebe86dcd0
Mia Erbus <mia.erbus@gmail.com>
2020-04-15 22:26:03 +0200

    [part3] phonebook database step4

--- index.js
+++ index.js
@@ -20,31 +20,37 @@ app.use(express.static('build'))
 
 app.get('/info', (req, res) => {
   const date = new Date()
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people.</p><p>${date}</p>`
-  )
+
+  Person.find({})
+    .then((persons) => {
+      const num = Object.keys(persons).length
+      res.send(`<p>Phonebook has info for ${num} people.</p><p>${date}</p>`)
+    })
+    .catch((error) => next(error))
 })
 
-app.get('/api/persons', (req, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons.map((p) => p.toJSON()))
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons.map((p) => p.toJSON()))
+    })
+    .catch((error) => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person.toJSON())
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndRemove(request.params.id).then(() => {
-    response.status(204).end()
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -56,23 +62,53 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  // const nameAlreadyExists = persons.find((person) => person.name === body.name)
-
-  // if (nameAlreadyExists) {
-  //   return response.status(400).json({
-  //     error: 'name must be unique',
-  //   })
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson.toJSON())
-  })
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson.toJSON())
+    })
+    .catch((error) => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch((error) => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
